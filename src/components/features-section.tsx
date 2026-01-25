fdc815531f951644c9631c978fd0d91dfe675295
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-01-25 22:02:20 +0530

    Normal commit 5

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -7,6 +7,11 @@ import { Icons } from '@/components/ui/icons';
 import { AnimatedNumber } from '@/components/ui/animated-number';
 
 
+
+
+
+
+
 const FeaturesSection = () => {
   const features = [
     {
