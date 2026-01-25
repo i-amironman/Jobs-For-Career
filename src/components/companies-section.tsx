f10dc469e0b11f6393b2ed69a5a26f3058a25ce3
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-01-25 22:00:32 +0530

    Normal commit 3

--- src/components/companies-section.tsx
+++ src/components/companies-section.tsx
@@ -6,14 +6,6 @@ import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 
 
-
-
-
-
-
-
-
-
 const CompaniesSection = () => {
   const topCompanies = [
     {
