24d6daf829e39b046039f30d38e82ca6ade42043
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-01-27 22:16:09 +0530

    initial commit 2

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -5,13 +5,6 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 
-
-
-
-
-
-
-
 interface ErrorBoundaryState {
   hasError: boolean;
   error?: Error;
