c397eeadb877a0553fa68baccccef35f09c60443
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-02-05 22:09:06 +0530

    failure error 1

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -7,6 +7,19 @@ import { Icons } from '@/components/ui/icons';
 import { AnimatedNumber } from '@/components/ui/animated-number';
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 const FeaturesSection = () => {
   const features = [
     {
