39ab5301fa2fe9f249d30874a36e7e66a7c1dcbf
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-02-07 22:01:31 +0530

    error 4

--- src/components/testimonials-section.tsx
+++ src/components/testimonials-section.tsx
@@ -6,6 +6,21 @@ import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Icons } from '@/components/ui/icons';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const TestimonialsSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
