f6772e0820e734148d7bb6db9dc8ceb371838078
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-02-09 18:11:53 +0530

    issue 1

--- src/components/testimonials-section.tsx
+++ src/components/testimonials-section.tsx
@@ -7,6 +7,23 @@ import { Badge } from '@/components/ui/badge';
 import { Icons } from '@/components/ui/icons';
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const TestimonialsSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
