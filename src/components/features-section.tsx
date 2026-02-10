1315af8b78184aa2b0bfe82348c2257eae29af0a
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-02-10 20:47:09 +0530

    warning 1

--- src/components/features-section.tsx
+++ src/components/features-section.tsx
@@ -6,6 +6,23 @@ import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 import { AnimatedNumber } from '@/components/ui/animated-number';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const FeaturesSection = () => {
   const features = [
     {
