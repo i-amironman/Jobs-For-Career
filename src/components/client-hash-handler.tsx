c080e7eae01a95e7d6a3e9f2322e66c7fe14fb57
i-amironman <lakkireddysathishreddy17@gmail.com>
2026-02-11 22:16:07 +0530

    general 6

--- src/components/client-hash-handler.tsx
+++ src/components/client-hash-handler.tsx
@@ -2,6 +2,22 @@
 
 import { useEffect } from 'react';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default function ClientHashHandler() {
   useEffect(() => {
     // Handle hash navigation on page load
